1b52f32f3071560260b7ac931ade1d518b058e29
dohyeopsong <vlsms119@wku.ac.kr>
2025-07-06 10:50:25 +0900

    Complete , turtule, draw a random colour.

--- turtle_graphic_GUI/draw_random_walk/main.py
+++ turtle_graphic_GUI/draw_random_walk/main.py
@@ -2,12 +2,19 @@ import turtle as t
 import random
 
 tim = t.Turtle()
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
-           "Wheat", "SlateGray", "SeaGreen"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 
 def random_walk(steps):
     for _ in range(steps):
-        tim.color(random.choice(colours))
+        tim.color(random_color())
         tim.forward(30)
         tim.setheading(random.choice([0, 90, 180, 270]))
 
@@ -16,4 +23,8 @@ def main():
     tim.pensize(10)
     tim.speed("fastest")
     random_walk(200)
-    t.done()      
+
+if __name__ == "__main__":
+    main()
+    screen = t.Screen()
+    screen.exitonclick()  # Click on the window to close it
