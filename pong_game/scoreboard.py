139cc929257f1554a80e2f9151bc937e5f69ff99
DohyeopSong <dohyeopsong@DohyeopSongui-MacBookAir.local>
2025-07-19 16:50:55 +0900

    delete useless code in pong and process turtle crossing game.

--- pong_game/scoreboard.py
+++ pong_game/scoreboard.py
@@ -3,7 +3,6 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.color("white")
         self.penup()
         self.hideturtle()
---
+++ turtle-crossing-start/main.py
@@ -0,0 +1,29 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+screen.onkey(player.move_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.01)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    if player.check_clear():
+        scoreboard.level_up()
