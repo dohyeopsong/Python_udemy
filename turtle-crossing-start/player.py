0799d90e8e48ab9835811cd453efb50b5210b0cf
DohyeopSong <dohyeopsong@DohyeopSongui-MacBookAir.local>
2025-07-20 15:05:48 +0900

    Compeleted project named turtle crossing game.

--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -4,21 +4,24 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+
 class Player(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.penup()
+        self.go_to_start()
         self.setheading(90)
-        self.goto(STARTING_POSITION)
 
-    def move_up(self):
+    def go_up(self):
         self.forward(MOVE_DISTANCE)
 
-    def move_down(self):
-        self.forward(-1 * MOVE_DISTANCE)
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
 
-    def check_clear(self):
+    def is_at_finish_line(self):
         if self.ycor() > FINISH_LINE_Y:
-            self.goto(STARTING_POSITION)
             return True
+        else:
+            return False
